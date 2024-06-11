dda0c70759881479a23b052b9057b42eb34f133b
Hansibaba <46038638+Hansibaba@users.noreply.github.com>
2024-06-11 18:41:15 -0400

    cloud sync - Worked on Card Controller

--- html/lich-card-editor.php
+++ html/lich-card-editor.php
@@ -54,36 +54,42 @@ else
                     <div class="col-md-6">
                         <div class="form-section">
                             <form>
-                                <div class="form-group">
-                                    <label for="cardName">Name</label>
-                                    <input type="text" class="form-control" id="cardName" placeholder="Enter card name" value=<?= $thisCard->name?>>
+                                <div class="form-row">
+                                    `<div class="form-group">
+                                        <label for="cardName">Name</label>
+                                        <input type="text" class="form-control" id="cardName" placeholder="Enter card name" value=<?= $thisCard->name?>>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="cardDescription">Description</label>
+                                        <textarea class="form-control" id="cardDescription" rows="3" placeholder="Enter card description" value=<?=$thisCard->description?>></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="cardCost">Cost</label>
+                                        <input type="number" class="form-control" id="cardCost" placeholder="Enter card cost" value=<?=$thisCard->cost?>>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="cardType">Type</label>
+                                        <select class="form-control" id="cardType">
+                                            <option value="ACT">ACT</option>
+                                            <option value="DEF">DEF</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="cardTeam">Team</label>
+                                        <select class="form-control" id="cardTeam">
+                                            <option value="Hunter">Hunter</option>
+                                            <option value="Lich">Lich</option>
+                                            <option value="Other">Other</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="cardImageId">Card Image ID</label>
+                                        <input type="number" class="form-control" id="cardImageId" placeholder="Enter card image ID">
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="cardDescription">Description</label>
-                                    <textarea class="form-control" id="cardDescription" rows="3" placeholder="Enter card description" value=<?=$thisCard->description?>></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="cardCost">Cost</label>
-                                    <input type="number" class="form-control" id="cardCost" placeholder="Enter card cost" value=<?=$thisCard->cost?>>
-                                </div>
-                                <div class="form-group">
-                                    <label for="cardType">Type</label>
-                                    <select class="form-control" id="cardType">
-                                        <option value="ACT">ACT</option>
-                                        <option value="DEF">DEF</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="cardTeam">Team</label>
-                                    <select class="form-control" id="cardTeam">
-                                        <option value="Hunter">Hunter</option>
-                                        <option value="Lich">Lich</option>
-                                        <option value="Other">Other</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="cardImageId">Card Image ID</label>
-                                    <input type="number" class="form-control" id="cardImageId" placeholder="Enter card image ID">
+                                
+                                <div class="form-row mt-4">
+                                    <button type="submit" class="btn btn-secondary">Save</button>
                                 </div>
                             </form>
                         </div>
@@ -94,7 +100,7 @@ else
                             <img src=<?=$thisCard->mediaPath?> alt="Card Image" class="card-image" id="cardImage">
                     </div>
                 </div>
-                <?php require("php-components/base-page-discord.php"); ?>
+
             </div>
             <?php require("php-components/base-page-footer.php"); ?>
     </main>
